e050437063e23d5027e1c6d42de870b9f114e3e4
xiaojin.pku@gmail.com <xiaojin.pku@gmail.com@2825a343-151f-30ab-5a30-a4c9441f2673>
2013-08-11 03:52:18 +0000

    加入一个统计

---
+++ cai/Algorithm/Sort.cs
@@ -0,0 +1,286 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace cai.Algorithm
+{
+    public class BubbleSort
+    {
+        List<int> _list;
+        public BubbleSort(List<int> sourceList)
+        {
+            _list = sourceList;
+        }
+
+        public void sort()
+        {
+            if (_list != null && _list.Count > 0)
+            {
+                for (int i = 1; i < _list.Count; i++)//趟数
+                {
+                    for (int j = _list.Count - 1; j >= i; j--)//比较
+                    {
+                        if (_list[j] > _list[j - 1])
+                        {
+                            int a = _list[j - 1];
+                            _list[j - 1] = _list[j];
+                            _list[j] = a;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public class BitMap
+    {
+        byte[] ByteArray;
+        List<int> _array;
+        static List<int> counter = new List<int>(8);
+
+        int columns
+        {
+            get
+            {
+                return ByteArray.Length;
+            }
+        }
+
+        public BitMap(List<int> orginial)
+        {
+            int total = GetMaxint(orginial.ToArray());
+            _array = orginial;
+
+            if (total % 8 != 0)
+            {
+                ByteArray = new byte[total / 8 + 1];
+            }
+            else
+            {
+                ByteArray = new byte[total / 8];
+            }
+        }
+
+        int GetPow(int number)
+        {
+            int ret = 0;
+            switch (number)
+            {
+                case 1:
+                    ret = 128;
+                    break;
+                case 2:
+                    ret = 64;
+                    break;
+                case 3:
+                    ret = 32;
+                    break;
+                case 4:
+                    ret = 16;
+                    break;
+                case 5:
+                    ret = 8;
+                    break;
+                case 6:
+                    ret = 4;
+                    break;
+                case 7:
+                    ret = 2;
+                    break;
+                case 8:
+                    ret = 1;
+                    break;
+            }
+            return ret;
+        }
+
+        int GetNumberInByte(int number)
+        {
+            int ret = 0;
+            switch (number)
+            {
+                case 1:
+                    ret = 8;
+                    break;
+                case 2:
+                    ret = 7;
+                    break;
+                case 3:
+                    ret = 6;
+                    break;
+                case 4:
+                    ret = 5;
+                    break;
+                case 5:
+                    ret = 4;
+                    break;
+                case 6:
+                    ret = 3;
+                    break;
+                case 7:
+                    ret = 2;
+                    break;
+                case 8:
+                    ret = 1;
+                    break;
+            }
+            return ret;
+        }
+
+        /// <summary>
+        /// 获取byte中实际位数号
+        /// </summary>
+        /// <param name="number"></param>
+        /// <param name="pow"></param>
+        protected void Mod(int number, int pow)
+        {
+            int pow1 = (int)System.Math.Pow(2, pow);
+
+            if (number == 0 || pow == -1)
+            {
+            }
+            else
+            {
+                if (number >= pow1)
+                {
+                    counter.Add(pow + 1);
+                    Mod(number % pow1, pow--);
+                }
+                else
+                {
+                    pow--;
+                    Mod(number, pow);
+                }
+            }
+        }
+        protected List<int> Howmany1InAByte(Byte b)
+        {
+            counter.Clear();
+            int i = Convert.ToInt32(b);
+            Mod(i, 7);
+            return counter;
+        }
+        protected List<int> HowMany(byte b)
+        {
+            int temp = (int)b;
+            int p = 0;
+            counter.Clear();
+            for (int i = 1; i <= 8; i++)
+            {
+                p = GetPow(i);
+                if ((temp | p) == temp)
+                {
+                    counter.Add(i);
+                }
+            }
+            return counter;
+        }
+
+        protected int GetMaxint(int[] Array)
+        {
+            int max = 0;
+
+            if (Array != null && Array.Length > 0)
+            {
+                max = Array[0];
+                for (int i = 1; i < Array.Length; i++)
+                {
+                    if (Array[i] < 0)
+                    {
+                        throw new Exception("No negative is allowed!");
+                    }
+                    if (Array[i] > max)
+                    {
+                        max = Array[i];
+                    }
+                }
+            }
+            else
+            {
+                throw new Exception("Array[] is null or empty!");
+            }
+            return max;
+        }
+
+
+        List<int> duplicateList = new List<int>();
+        public void sort()
+        {
+            List<int> ret = new List<int>();
+            bool flag = false;
+
+            foreach (int i in _array)
+            {
+                int col = i / 8;
+                int mod = i % 8;
+                if (mod == 0)
+                {
+                    col -= 1;
+                    mod = 8;
+                }
+                byte tempByte = Convert.ToByte(GetPow(mod));
+
+                if (ByteArray[col] != 0)
+                {
+                    flag = false;//默认是不要去重复值
+                    //List<int> t1 = Howmany1InAByte(ByteArray[col]);
+                    //foreach (int ii in t1)
+                    //{
+                    //    /*Howmany1InAByte是从右往左数
+                    //     mod是从左往右数，所以必须转换*/
+                    //    if ((8 - ii + 1) == mod) { flag = true; duplicateList.Add(i); }
+                    //}
+
+                    if (((int)ByteArray[col] | (int)tempByte) == (int)ByteArray[col])
+                    {
+                        flag = true;
+                        duplicateList.Add(i);
+                    }
+
+                    if (!flag)
+                    {
+                        ByteArray[col] += tempByte;
+                        tempByte = 0;
+                    }
+                }
+                else
+                {
+                    ByteArray[col] += tempByte;
+                    tempByte = 0;
+                }
+            }
+
+            for (int i = ByteArray.Length - 1; i >= 0; i--)
+            {
+                if (ByteArray[i] != 0)
+                {
+                    // List<int> t2=Howmany1InAByte(ByteArray[i]);
+                    List<int> t2 = HowMany(ByteArray[i]);
+                    for (int j = t2.Count - 1; j >= 0; j--)
+                    {
+                        ret.Add(i * 8 + t2[j]);
+                        //ret.Add(i * 8 +GetNumberInByte(t2[j]));
+                    }
+                }
+            }
+
+            int c = 0;
+            foreach (int i in ret)
+            {
+                _array[c] = i;
+                c++;
+            }
+            for (; c < _array.Count; c++)
+            {
+                _array[c] = -1;
+            }
+
+            Console.Write("Duplicated List: ");
+            foreach (int d in duplicateList)
+            {
+                Console.Write(d.ToString() + ", ");
+            }
+        }
+    }
+}
---
+++ cai/Report/BasicReport.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace cai.Report
+{
+    public class BasicReport:IReport
+    {
+        static log4net.ILog LOGGER = null;
+        static BasicReport()
+        {
+            if (LOGGER == null)
+            {
+                LOGGER = log4net.LogManager.GetLogger(typeof(BasicReport));
+            }
+        }
+        public void Compute()
+        {
+            if (LOGGER.IsInfoEnabled)
+            {
+                DoubleBoll[] allBalls = DBHelper.GetALL<DoubleBoll>(MainWindow._db, "_No");
+                if (allBalls != null && allBalls.Length > 0)
+                {
+                    LOGGER.Info("================================Basic Report===============================");
+                    int[] Redslots = new int[33];
+                    int[] Blueslots = new int[16];
+                    foreach (DoubleBoll boll in allBalls)
+                    {
+                        Redslots[boll.Red1 - 1] += 1;
+                        Redslots[boll.Red2- 1] += 1;
+                        Redslots[boll.Red3 - 1] += 1;
+                        Redslots[boll.Red4 - 1] += 1;
+                        Redslots[boll.Red5 - 1] += 1;
+                        Redslots[boll.Red6 - 1] += 1;
+
+                        Blueslots[boll.Blue - 1] += 1;
+                    }
+
+                    for (int i = 0; i < Redslots.Length; i++)
+                    {
+                        LOGGER.Info("红" + (i+1).ToString() +" 共："+ Redslots[i].ToString()+"\n");
+                    }
+
+                    for (int i = 0; i < Blueslots.Length; i++)
+                    {
+                        LOGGER.Info("蓝" + (i+1).ToString() + " 共：" + Blueslots[i].ToString() + "\n");
+                    }
+
+                    Dictionary<int, List<string>> _redDic = new Dictionary<int, List<string>>();
+                    List<int> _redCishu = new List<int>();
+                    for (int i = 0; i < Redslots.Length; i++)
+                    {
+                        string redno = (i + 1).ToString();
+                        if (_redDic.ContainsKey(Redslots[i]))
+                            _redDic[Redslots[i]].Add(redno);
+                        else
+                        {
+                            List<string> _redlist = new List<string>();
+                            _redlist.Add(redno);
+                            _redDic.Add(Redslots[i], _redlist);
+                        }
+
+                        if(!_redCishu.Contains(Redslots[i]))
+                            _redCishu.Add(Redslots[i]);
+                    }
+
+                    Dictionary<int, List<string>> _blueDic = new Dictionary<int, List<string>>();
+                    List<int> _blueCishu = new List<int>();
+                    for (int i = 0; i < Blueslots.Length; i++)
+                    {
+                        string blueno = (i + 1).ToString();
+                        if (_blueDic.ContainsKey(Blueslots[i]))
+                            _blueDic[Blueslots[i]].Add(blueno);
+                        else
+                        {
+                            List<string> _bluelist = new List<string>();
+                            _bluelist.Add(blueno);
+                            _blueDic.Add(Blueslots[i], _bluelist);
+                        }
+
+                        if(!_blueCishu.Contains(Blueslots[i]))
+                            _blueCishu.Add(Blueslots[i]);
+                    }
+
+                    cai.Algorithm.BitMap Redsort = new Algorithm.BitMap(_redCishu);
+                    Redsort.sort();
+
+                    cai.Algorithm.BitMap Bluesort = new Algorithm.BitMap(_blueCishu);
+                    Bluesort.sort();
+
+                    LOGGER.Info("红球排序输出\n");
+                    foreach (int key in _redCishu)
+                    {
+                        foreach (string s in _redDic[key])
+                        {
+                            LOGGER.Info("红:" + s+" "+key.ToString()+"\n");
+                        }
+                    }
+
+                    LOGGER.Info("篮球排序输出\n");
+                    foreach (int key in _blueCishu)
+                    {
+                        foreach (string s in _blueDic[key])
+                        {
+                            LOGGER.Info("蓝:" + s + " " + key.ToString() + "\n");
+                        }
+                    }
+
+                        LOGGER.Info("================================END Basic Report===============================");
+                }
+            }
+        }
+    }
+}
